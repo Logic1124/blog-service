b4b8e2c976851d324cffe6eab3f4f9bbeb25d59b
lvbu <logic1124@163.com>
2024-02-28 17:32:14 +0800

    [add] 进行到日志写入

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"fmt"
+	"github.com/Logic1124/blog-service/global"
+	"github.com/Logic1124/blog-service/pkg/setting"
+	"github.com/jinzhu/gorm"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -17,4 +24,27 @@ type Tag struct {
 
 func (t Tag) TableName() string {
 	return "blog_tag"
+}
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
+	db, err := gorm.Open(databaseSetting.DBType, fmt.Sprintf(s,
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
+
+	return db, nil
 }
